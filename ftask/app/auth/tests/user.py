4490da28cc1b2675a742cbe333b287b902cbcf96
Daniel Garcia Moreno <danigm@wadobo.com>
2012-12-26 10:53:35 +0100

    Test: cleaning auth test code

--- ftask/app/auth/tests/user.py
+++ ftask/app/auth/tests/user.py
@@ -5,20 +5,30 @@ from ...tests.base import BaseTestCase
 
 
 class UserTestCase(BaseTestCase):
+    PATH = '/api/users'
 
-    def test_users_register(self):
+    def test_register(self):
         # Bad data
         data = { 'username': 'danigm' }
-        res = self.app.post('/api/users/register/', data=data)
-        self.assertEqual(res.status_code, 400)
+        res = self.post('/register/', data=data, status=400)
 
         # all ok
         data = { 'username': 'danigm',
                  'password': '123',
                  'email': 'danigm@wadobo.com'}
-        res = self.app.post('/api/users/register/', data=data)
-        self.assertEqual(res.status_code, 200)
+        res = self.post('/register/', data=data, status=200)
 
         # already registered user
-        res = self.app.post('/api/users/register/', data=data)
-        self.assertEqual(res.status_code, 400)
+        res = self.post('/register/', data=data, status=400)
+
+
+    def test_list(self):
+        res = self.get('/', status=200, tojson=True)
+        self.assertEqual(res.json['meta']['total'], 0)
+
+        # adding people
+        self.test_register()
+
+        res = self.get('/', status=200, tojson=True)
+        self.assertEqual(res.json['meta']['total'], 1)
+        self.assertEqual(res.json['objects'][0]['username'], 'danigm')
--- ftask/app/tests/base.py
+++ ftask/app/tests/base.py
@@ -2,6 +2,8 @@
 
 from __future__ import division, absolute_import
 
+from flask import json
+
 import ftask
 import unittest
 
@@ -17,3 +19,35 @@ class BaseTestCase(unittest.TestCase):
 
     def tearDown(self):
         MongoClient().drop_database('ftask_test')
+
+    def get(self, path, status=200, tojson=False):
+        res = self.app.get(self.PATH + path)
+        self.assertEqual(res.status_code, status)
+        if tojson:
+            res.json = json.loads(res.data)
+
+        return res
+
+    def post(self, path, data={}, status=200, tojson=False):
+        res = self.app.post(self.PATH + path, data=data)
+        self.assertEqual(res.status_code, status)
+        if tojson:
+            res.json = json.loads(res.data)
+
+        return res
+
+    def put(self, path, data={}, status=200, tojson=False):
+        res = self.app.put(self.PATH + path, data=data)
+        self.assertEqual(res.status_code, status)
+        if tojson:
+            res.json = json.loads(res.data)
+
+        return res
+
+    def delete(self, path, data={}, status=200, tojson=False):
+        res = self.app.delete(self.PATH + path, data=data)
+        self.assertEqual(res.status_code, status)
+        if tojson:
+            res.json = json.loads(res.data)
+
+        return res
