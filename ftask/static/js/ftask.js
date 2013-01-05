1077ff7754016cfa93899afdb64e7f10dde61756
Daniel Garcia Moreno <danigm@wadobo.com>
2013-01-05 19:17:05 +0100

    utils.js changed to ftask.js and created a namespace

---
+++ ftask/static/js/ftask.js
@@ -0,0 +1,76 @@
+(function() {
+    var Ftask = this.Ftask = {};
+
+    Ftask.baseApi = '/api';
+    Ftask.baseApiAuth = Ftask.baseApi + '/users';
+    Ftask.baseApiBoard = Ftask.baseApi + '/boards';
+
+    Ftask.ldg = 0;
+    // Shows a  loading animation
+    Ftask.loading = function (loading) {
+        if (loading) {
+            Ftask.ldg++;
+            $("#mini-loading").fadeIn();
+        } else {
+            Ftask.ldg--;
+            if (Ftask.ldg <= 0) {
+                $("#mini-loading").fadeOut();
+                Ftask.ldg = 0;
+            }
+        }
+    }
+
+    // Queries the csrf token to the api
+    Ftask.updateCsrf = function () {
+        Ftask.loading(true);
+        var req = $.get(Ftask.baseApiAuth + '/csrf_token/', {}, function(data) {
+            $("#csrf_token").val(data.token);
+            Ftask.loading(false);
+        });
+    }
+
+    // Convert a html form in a ajax query to the api
+    Ftask.form = function (id, onerror, onsuccess) {
+
+        function done () {
+            if ($("#csrf_token").length) {
+                Ftask.updateCsrf();
+            }
+            $("#loading").fadeOut();
+        }
+
+        $(id).submit(function() {
+            var url = $(id).data("url");
+            var data = $(id).serialize();
+            var method = $(id).attr("method");
+
+            if ($("#csrf_token").length) {
+                data += "&_csrf_token="+$("#csrf_token").val();
+            }
+
+            $("#loading").fadeIn();
+            var req = $.ajax({url:url, data:data, type:method});
+
+            // DONE
+            req.done(function(data) {
+                var next = $(id).data("next");
+                if (next && data.status === "success") {
+                    window.location.href = next;
+                } else {
+                    onsuccess(data);
+                }
+
+                done();
+            });
+
+            // ERROR
+            req.fail(function(data) {
+                onerror(data);
+                done();
+            });
+
+            return false;
+        });
+    }
+
+}).call(this);
